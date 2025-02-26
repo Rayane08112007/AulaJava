c94634c5bcd184b27e2192e629fd28a2470dba1d
Rayane08112007 <rayane.tahara@etec.sp.gov.br>
2025-02-26 20:28:15 -0300

    Aula Java 3 semana
    Criação de metodos

---
+++ AulaJavaMetodo/src/Rayane/metodo/Calculadora.java
@@ -0,0 +1,31 @@
+package Rayane.metodo;
+
+public class Calculadora {
+   
+    int number1 = 10;
+    int number2 = 10;
+    
+    void adicao(){
+        
+        int resultado = number1 + number2;
+        System.out.println("o valor da soma é: "+ resultado);
+        
+    
+        
+    }void subtracao(){
+        int resultado = number1 - number2;
+        System.out.println("o valor da soma é: "+ resultado);
+        
+    }void multiplicacao(){
+        int resultado = number1 * number2;
+        System.out.println("o valor da soma é: "+ resultado);
+        
+    }void divisao(){
+        int resultado = number1 / number2;
+        System.out.println("o valor da soma é: "+ resultado);
+        
+        
+    }
+    
+    
+}
---
+++ AulaJavaMetodo/src/Rayane/metodo/DigitarScanner.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rayane.metodo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RAYANE_4779
+ */
+public class DigitarScanner {
+    public static void main (String[]args){
+           Scanner x = new Scanner (System.in);
+           
+           System.out.println("Digite o seu primeiro valor");
+           int number1 = x.nextInt();
+           System.out.println("");
+           int.
+            System.out.println("Digite o seu segundo valor");
+           int number2 = x.nextInt();
+           
+        
+           
+           int resultado = number1 + number2;
+           System.out.println("o valor da soma é: "+ resultado);
+           
+           System.out.println();
+           
+          
+}
+}
